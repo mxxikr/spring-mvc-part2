1c476d1daeeb6600b1d7f8cab336fbcbacc512fe
mxxikr <mxxikr@gmail.com>
2026-01-26 20:25:58 +0900

    검증1 Validation: Validator 분리2

--- validation/src/main/java/hello/itemservice/web/validation/ItemValidator.java
+++ validation/src/main/java/hello/itemservice/web/validation/ItemValidator.java
@@ -20,10 +20,6 @@ public class ItemValidator implements Validator {
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "itemName", "required");
 
-        if (!StringUtils.hasText(item.getItemName())) {
-            errors.rejectValue("itemName", "required");
-        }
-
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
             errors.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
         }
